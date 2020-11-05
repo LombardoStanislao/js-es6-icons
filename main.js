054d27473df1ad52b20efc82d33f4951bd91a2d4
Stanislao <stanislaoweb@libero.it>
2020-11-05 15:41:35 +0100

    Finito Milestone 1, preparato il 2

--- main.js
+++ main.js
@@ -1,5 +1,132 @@
+// milestone 1:
+// definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
+
+// milestone 2:
+// definire un array di colori e associare ad ogni tipo di icona un colore.
+// Visualizzare le icone di colore diverso in base al tipo.
+
 $(document).ready(function(){
 
+  const icons = [
+      {
+          name: 'cat',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'crow',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'dog',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'dove',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'dragon',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'horse',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'hippo',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'fish',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas'
+      },
+      {
+          name: 'carrot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas'
+      },
+      {
+          name: 'apple-alt',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas'
+      },
+      {
+          name: 'lemon',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas'
+      },
+      {
+          name: 'pepper-hot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas'
+      },
+      {
+          name: 'user-astronaut',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas'
+      },
+      {
+          name: 'user-graduate',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas'
+      },
+      {
+          name: 'user-ninja',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas'
+      },
+      {
+          name: 'user-secret',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas'
+      }
+  ];
 
-  alert('ciao')
+  const colors = ['green', 'coral', 'yellow'];
+// Seleziono ogni singolo oggetto per estrarne gli attributi
+
+  icons.forEach((icon, i) => {
+
+    const {name, prefix, type, family} = icon;
+
+    $('#all-icons').append(`
+
+        <i class="${family} ${prefix}${name} fa-2x" </i>
+
+
+      `
+
+    )
+
+  });
+
+
+
+
+
 });
